efd75b8755dc19907baf5fb9daf013a6d7912302
jcc <changejian@gmail.com>
2017-03-11 21:45:30 +0800

    Add tests for article's api and user's api

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\User;
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function actingAsAdmin()
+    {
+        Passport::actingAs(
+            factory(User::class, 'admin')->create(),
+            ['user', 'article']
+        );
+    }
 }
