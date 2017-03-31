b0ff4d49749ece79cfee479366b798e3102bda76
jcc <changejian@gmail.com>
2017-03-31 11:57:45 +0800

    Deleted Jumbotron.vue component

---
+++ resources/views/particals/jumbotron.blade.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="jumbotron text-center">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
