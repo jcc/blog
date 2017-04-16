cdeddbe4fddae701ecbc822988a5ff71b1a82501
jcc <changejian@gmail.com>
2017-04-16 21:30:06 +0800

    Fixed the discussion page blank in some cases

---
+++ resources/assets/js/components/Textarea.vue
@@ -0,0 +1,14 @@
+<template>
+    <textarea class="form-control" id="content" rows="12" name="content" v-text="content"></textarea>
+</template>
+
+<script>
+export default {
+    props: {
+        content: {
+            type: String,
+            default: null
+        }
+    }
+}
+</script>
