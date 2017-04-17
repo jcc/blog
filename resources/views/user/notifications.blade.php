2b84105f00d41a62a54648411e043e41ce20cebc
vagrant <vagrant@homestead>
2017-04-17 07:30:33 +0000

    Mod format and add function notes

--- resources/views/user/notifications.blade.php
+++ resources/views/user/notifications.blade.php
@@ -6,23 +6,19 @@
             <div class="col-md-4">
                 @include('setting.particals.sidebar')
             </div>
-
             <div class="col-md-8">
                 <div class="panel">
                     <div class="panel-heading">
-                    <span>{{ Auth::user()->unreadNotifications->count() }}</span>
-                    {{ lang('New Notification') }}
-        <a class="pull-right mark-as-read" href="#"
-           onclick="event.preventDefault();
-                                                     document.getElementById('mark-as-read').submit();">
-            {{ lang('Mark As Read') }}
-        </a>
-
-        <form id="mark-as-read" action="{{ url('user/notification') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-        </form>
+                        <span>{{ Auth::user()->unreadNotifications->count() }}</span>
+                        {{ lang('New Notification') }}
+                        <a class="pull-right mark-as-read" href="#"
+                           onclick="event.preventDefault();document.getElementById('mark-as-read').submit();">
+                            {{ lang('Mark As Read') }}
+                        </a>
+                        <form id="mark-as-read" action="{{ url('user/notification') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
                     </div>
-
                     <div class="panel-body">
                         <div class="row notification">
                             @foreach(Auth::user()->notifications as $notification)
