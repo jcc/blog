1608ca894caf582be128c6e5a6ac4afabe55e672
jcc <changejian@gmail.com>
2017-07-06 21:13:41 +0800

    Upgrade Translug Package

--- config/services.php
+++ config/services.php
@@ -44,8 +44,8 @@ return [
 
     // Translation
     'youdao' => [
-        'key' => env('YOUDAO_API_KEY'),
-        'from' => env('YOUDAO_KEY_FROM'),
+       'appKey' => env('YOUDAO_APP_KEY'),
+       'appSecret' => env('YOUDAO_APP_SECRET'),
     ],
 
 ];
