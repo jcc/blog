ced1d014487e55724b1a1a29bd0e7689f1906dab
jcc <changejian@gmail.com>
2018-03-01 11:28:52 +0800

    Update file path

--- resources/assets/js/dashboard/modules/comment/routes.js
+++ resources/assets/js/dashboard/modules/comment/routes.js
@@ -1,6 +1,6 @@
 export default [{
   path: 'comments',
-  component: () => import('js/App'),
+  component: () => import('js/App.vue'),
   children: [{
     path: '/',
     name: 'dashboard.comment',
