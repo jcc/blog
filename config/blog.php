8f4600a17d38b331cf212a2cad88c3cc0c9a698f
冯帅 <wsnbb@163.com>
2018-10-30 15:44:18 +0800

    更新网站的相关信息，如Meta等

--- config/blog.php
+++ config/blog.php
@@ -19,8 +19,8 @@ return [
 
     // Meta
     'meta' => [
-        'keywords' => 'PJ Blog,blog,pigjian,laravel,vuejs',
-        'description' => 'Nothing is impossible in PJ Blog'
+        'keywords' => 'PhpGoto,phpgoto,laravel,vuejs,linux,python',
+        'description' => 'Nothing is impossible in PhpGoto'
     ],
 
     // Social Share
@@ -40,7 +40,7 @@ return [
     // Article Page
     'article' => [
         'title'       => 'Nothing is impossible.',
-        'description' => 'https://pigjian.com',
+        'description' => '',
         'number'      => 15,
         'sort'        => 'desc',
         'sortColumn'  => 'published_at',
@@ -57,15 +57,15 @@ return [
     'footer' => [
         'github' => [
             'open' => true,
-            'url'  => 'https://github.com/jcc',
+            'url'  => 'https://github.com/skywingfs',
         ],
         'twitter' => [
-            'open' => true,
+            'open' => false,
             'url'  => 'https://twitter.com/pigjian'
         ],
-        'meta' => '© PJ Blog 2016. Powered By Jiajian Chan',
+        'meta' => '© Blog Powered By Jiajian Chan',
     ],
 
-    'license' => 'Powered By Jiajian Chan.<br/>This article is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.',
+    'license' => 'Powered By PhpGoto.<br/>This article is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.',
 
 ];
