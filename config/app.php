9a8d8e519df4159e318aee94859f040dbe39ab41
冯帅 <wsnbb@163.com>
2018-11-02 10:43:30 +0800

    test

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APPLICATION_NAME') ?: 'PHP GOTO',
+    'name' => env('APPLICATION_NAME') ?: 'PJ Blog',
 
     /*
     |--------------------------------------------------------------------------
