e1e0afcce65797e35d3ca88ac3813171801ce62a
冯帅 <wsnbb@163.com>
2018-11-02 10:42:19 +0800

    update

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://phpgoto.com'),
+    'url' => env('APP_URL', 'http://pigjian.app'),
 
     /*
     |--------------------------------------------------------------------------
