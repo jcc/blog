97e7e3dafd6e6f2ccb0ff3630bdda1cfa84f2ee6
冯帅 <wsnbb@163.com>
2018-11-09 12:32:08 +0800

    自动备份定时任务

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('02:00');
     }
 
     /**
