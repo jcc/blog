6f7d1247b4ba0930a66afb3dfac4dab4007c8fe0
冯帅 <wsnbb@163.com>
2018-12-26 10:24:18 +0800

    update 前端加载优化

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+const {mix} = require('laravel-mix');
 const path = require('path');
 
 /*
@@ -12,48 +12,78 @@ const path = require('path');
  |
  */
 let config = {
-  resolve: {
-    alias: {
-      'config': 'js/config',
-      'lang': 'js/lang',
-      'plugins': 'js/plugins',
-      'vendor': 'js/vendor',
-      'dashboard': 'js/dashboard',
-      'home': 'js/home',
-      'js': 'js',
+    resolve: {
+        alias: {
+            'config': 'js/config',
+            'lang': 'js/lang',
+            'plugins': 'js/plugins',
+            'vendor': 'js/vendor',
+            'dashboard': 'js/dashboard',
+            'home': 'js/home',
+            'js': 'js',
+        },
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, "resources")
+        ]
     },
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, "resources")
-    ]
-  },
 }
 
 if (!process.argv.includes('--hot')) {
-  config = Object.assign(config, {
-    output: {
-      publicPath: "/",
-      chunkFilename: 'js/[name].[chunkhash].js'
-    }
-  })
+    config = Object.assign(config, {
+        output: {
+            publicPath: "/",
+            chunkFilename: 'js/[name].[chunkhash].js'
+        }
+    })
 }
 
 mix.webpackConfig(config)
 
 let themes = [
-  'resources/sass/themes/default-theme.scss',
-  'resources/sass/themes/gray-theme.scss',
+    'resources/sass/themes/default-theme.scss',
+    'resources/sass/themes/gray-theme.scss',
 ];
 
 themes.forEach((item) => {
-  mix.sass(item, 'public/css/themes')
+    mix.sass(item, 'public/css/themes')
 })
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
-  .js('resources/js/home.js', 'public/js')
-  .sass('resources/sass/home.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/home.js', 'public/js')
+    .sass('resources/sass/home.scss', 'public/css')
+    // .js('resources/js/vendor/jquery-3.3.1.min.js', 'public/js')
+    // .js('resources/js/vendor/imagesloaded.min.js', 'public/js')
+    // .js('resources/js/vendor/masonry.js', 'public/js')
 
 if (mix.inProduction()) {
-  mix.version()
+    mix.version()
+    mix.webpackConfig({
+        plugins: [
+            new OptimizeCSSPlugin({
+                cssProcessorOptions: {
+                    safe: true,
+                    discardComments: {
+                        removeAll: true
+                    }
+                }
+            })
+        ]
+    });
+    mix.options({
+        uglify: {
+            uglifyOptions: {
+                sourceMap: false, // 关闭资源映射
+                compress: {
+                    warnings: false,
+                    drop_console: true // 去除控制台输出代码
+                },
+                output: {
+                    comments: false // 去除所有注释
+                }
+            }
+        }
+    });
+
 }
