d2eb88fdc61b6b19e102aca05259c3f0583de5c2
daydevelops <adamday@daydevelops.com>
2020-03-17 15:25:42 -0230

    updated header display in series views

--- resources/views/series/show.blade.php
+++ resources/views/series/show.blade.php
@@ -1,16 +1,13 @@
 @extends('layouts.app')
-@section('header')
-    @component('particals.jumbotron')
-        <h1 class='oleo'>{{ $series->name }}</h1>
-
-        <h3>{{ $series->description }}</h3>
-    @endcomponent
-@endsection
 @section('content')
 
-    @include('widgets.article')
+@component('particals.jumbotron')
+<h1>{{ $series->name }}</h1>
 
-    {{ $articles->links('pagination.default') }}
+<h3>{{ $series->description }}</h3>
+@endcomponent
+@include('widgets.article')
+
+{{ $articles->links('pagination.default') }}
 
 @endsection
-
